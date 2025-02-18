a0582bab0de613cb542510435e15517e23020102
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2025-02-18 16:54:48 -0300

    Add files via upload

---
+++ site_receitas/btn.js
@@ -0,0 +1,49 @@
+function VISUALIZAR1() {
+    
+    window.location.href = "file:///C:/Users/LAB4/Desktop/site_receitas/receita1.html";
+}
+
+function VISUALIZAR2() {
+    
+    window.location.href = "file:///C:/Users/LAB4/Desktop/site_receitas/receita2.html";
+}
+
+function VISUALIZAR3() {
+    
+    window.location.href = "file:///C:/Users/LAB4/Desktop/site_receitas/receita3.html";
+}
+
+function VISUALIZAR4() {
+    
+    window.location.href = "file:///C:/Users/LAB4/Desktop/site_receitas/receita4.html";
+}
+
+function VISUALIZAR5() {
+    
+    window.location.href = "file:///C:/Users/LAB4/Desktop/site_receitas/receita5.html";
+}
+
+function VISUALIZAR6() {
+   
+    window.location.href = "file:///C:/Users/LAB4/Desktop/site_receitas/receita6.html";
+}
+
+function VISUALIZAR7() {
+    
+    window.location.href = "file:///C:/Users/LAB4/Desktop/site_receitas/receita7.html";
+}
+
+function VISUALIZAR8() {
+    
+    window.location.href = "file:///C:/Users/LAB4/Desktop/site_receitas/receita8.html";
+}
+
+function VISUALIZAR9() {
+    
+    window.location.href = "file:///C:/Users/LAB4/Desktop/site_receitas/receita9.html";
+}
+
+function VISUALIZAR10() {
+    
+    window.location.href = "file:///C:/Users/LAB4/Desktop/site_receitas/receita10.html";
+}
